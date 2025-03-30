70e734ae72455bd6a3d59f6461b20c58a1e85d4d
Lindokuhle Mdlalose <mdllin028@myuct.ac.za>
2025-03-30 22:51:47 +0000

    Started the the implementation of the main function

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "PGMimageProcessor.h"
+
+int main(int argc, int argv[]){
+    if (argc < 2){
+        std::cerr << "Usage: " << argv[0] << " [options] <inputPGMfile>\n";
+        return 1;
+    }
+    
+}
