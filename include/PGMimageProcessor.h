9d420dcf22da94c7b219f2162100c94d34a02f60
Lindokuhle Mdlalose <mdllin028@myuct.ac.za>
2025-03-30 19:39:06 +0000

    Defined the structure of the PGMimageProcessor class, by implementing its header file

---
+++ include/PGMimageProcessor.h
@@ -0,0 +1,34 @@
+#ifndef PGMIMAGEPROCESSOR_H
+#define PGMIMAGEPROCESSOR_H
+
+class PGMimageProcessor{
+    private:
+        std::vector<std::unique_ptr<ConnectedComponent>> components;
+        unsigned char* imageData;
+        int width;
+        int height;
+
+    public:
+        explicit PGMimageProcessor(const std::string& filename);
+        ~PGMimageProcessor();
+
+        PGMimageProcessor(const PGMimageProcessor&) = delete; //no copying
+        PGMimageProcessor(PGMimageProcessor&&) noexcept;
+        PGMimageProcessor& operator=(const PGMimageProcessor&) = delete;
+        PGMimageProcessor& operator=(PGMimageProcessor&&) noexcept;
+
+        int extractComponents(unsigned char threshold, int minValidSize);
+        int filterComponentsBySize(int minSize, int maxSize);
+        bool writeComponents(const std::string& outFilename);
+        int getComponentCount() const;
+        int getLargestSize() const;
+        int getSmallestSize() const;
+        void printComponentData(const ConnectedComponent& component) const;
+
+    private:
+        //helper methods
+        bool readPGMFile(const std::string& filename);
+        void BFS(int x, int y, unsigned char threshold, std::unique_ptr<ConnectedComponent>& components);
+};
+
+#endif
