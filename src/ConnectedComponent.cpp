21ba2d7b7e7570fe99bca8d41848119e62d8eff0
Lindokuhle Mdlalose <mdllin028@myuct.ac.za>
2025-03-30 20:24:06 +0000

    Implemented the addPixel method

--- src/ConnectedComponent.cpp
+++ src/ConnectedComponent.cpp
@@ -4,3 +4,8 @@
 ConnectedComponent::ConnectedComponent() : id(-1), pixelCount(0){}
 
 ConnectedComponent::ConnectedComponent(int componentID) : id(componentID), pixelCount(0){}
+
+void ConnectedComponent::addPixel(int x, int y){
+    pixels.emplace_back(x, y);
+    pixelCount++;
+}
