f86874ba7b40a2e249e65df23757d411114f8ce2
Lindokuhle Mdlalose <mdllin028@myuct.ac.za>
2025-04-04 11:35:55 +0000

    Added a missing test case for the component size in the test file

--- src/test.cpp
+++ src/test.cpp
@@ -3,28 +3,34 @@
 #include "PGMimageProcessor.h"
 
 TEST_CASE("Custom Constructor", "[PGMimageProcessor]") {
-    PGMimageProcessor p("Assignment Resources/Shapes.pgm");
+    PGMimageProcessor p("pgm_test_files/Shapes.pgm");
     REQUIRE(p.getWidth() > 0);
     REQUIRE(p.getHeight() > 0);
     REQUIRE(p.getComponentCount() == 0);
 }
 
 TEST_CASE("Component Extraction", "[PGMimageProcessor]") {
-    PGMimageProcessor p("Assignment Resources/Shapes.pgm");
+    PGMimageProcessor p("pgm_test_files/Shapes.pgm");
     int count = p.extractComponents(128, 1);
     REQUIRE(count > 0);
     REQUIRE(p.getComponentCount() == count);
 }
 
 TEST_CASE("Largest and Smallest Component Size", "[PGMimageProcessor]") {
-    PGMimageProcessor p("Assignment Resources/Shapes.pgm");
+    PGMimageProcessor p("pgm_test_files/Shapes.pgm");
     p.extractComponents(128, 1);
     REQUIRE(p.getLargestSize() == 520187);
     REQUIRE(p.getSmallestSize() > 0);
 }
 
+TEST_CASE("Component Count", "[PGMimageProcessor]") {
+    PGMimageProcessor p("pgm_test_files/Shapes.pgm");
+    p.extractComponents(128, 1);
+    REQUIRE(p.getComponentCount() == 118);
+}
+
 TEST_CASE("Filter Components by Size", "[PGMimageProcessor]") {
-    PGMimageProcessor p("Assignment Resources/Shapes.pgm");
+    PGMimageProcessor p("pgm_test_files/Shapes.pgm");
     p.extractComponents(128, 1);
     int beforeFilter = p.getComponentCount();
     p.filterComponentsBySize(10000, 500000);
@@ -32,7 +38,7 @@ TEST_CASE("Filter Components by Size", "[PGMimageProcessor]") {
 }
 
 TEST_CASE("Write Components to File", "[PGMimageProcessor]") {
-    PGMimageProcessor p("Assignment Resources/Shapes.pgm");
+    PGMimageProcessor p("pgm_test_files/Shapes.pgm");
     p.extractComponents(128, 1);
     REQUIRE(p.writeComponents("output.pgm"));
 }
