e449dd0edf8dfb93979d7c8980dd6993e1f69cc6
Lindokuhle Mdlalose <mdllin028@myuct.ac.za>
2025-04-08 19:11:33 +0000

    Wrote the scope of the PPMimageProcessor class in its header file

--- src/PPMimageProcessor.h
+++ src/PPMimageProcessor.h
@@ -8,7 +8,42 @@
 #include <utility>
 
 class PPMimageProcessor {
-    
-};
+    private:
+        std::vector<std::unique_ptr<ConnectedComponent>> components;
+        unsigned char* imageData; //3 bytes per pixel (RGB)
+        int width;
+        int height;
 
+    public:
+        explicit PPMimageProcessor(const std::string& filename);
+        ~PPMimageProcessor();
+
+        //disallow copying
+        PPMimageProcessor(const PPMimageProcessor&) = delete;
+        PPMimageProcessor& operator=(const PPMimageProcessor&) = delete;
+
+        //allow moving 
+        PPMimageProcessor(PPMimageProcessor&& other) noexcept;
+        PPMimageProcessor& operator=(PPMimageProcessor&& other) noexcept;
+
+        //core functionality
+        int extractComponents(unsigned char threshold, int minValidSize);
+        int filterComponents(int minSize, int maxSize);
+        bool writeComponents(const std::string& outFileName);
+        bool writeBoxedComponents(const std::string& outFileName, unsigned char boxColor[3] = nullptr);
+
+        //getters
+        int getComponentCount() const;
+        int getLargestSize() const;
+        int getSmallestSize() const;
+        void printComponentData(const ConnectedComponent& component) const;
+        int getWidth() const { return width; };
+        int getHeight() const { return height; };
+
+    private:
+        //helper methods
+        bool readPPMFile(const std::string& filename);
+        void BFS(int x, int y, unsigned char threshold, bool* visited, std::unique_ptr<ConnectedComponent>& component);
+        unsigned char rgbToGrayScale(unsigned char r, unsigned char g, unsigned char b);
+};
 #endif
