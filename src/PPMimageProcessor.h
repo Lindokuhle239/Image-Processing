61f4f190d3f6725242a124630a9c31c1fb6c2b65
Lindokuhle Mdlalose <mdllin028@myuct.ac.za>
2025-04-08 19:02:10 +0000

    Initialized the implementation of the PPMimageProcessor header file

---
+++ src/PPMimageProcessor.h
@@ -0,0 +1,14 @@
+#ifndef PPMIMAGEPROCESSOR_H
+#define PPMIMAGEPROCESSOR_H
+
+#include "ConnectedComponent.h"
+#include <memory>
+#include <vector>
+#include <string>
+#include <utility>
+
+class PPMimageProcessor {
+    
+};
+
+#endif
