56d938de7d374eccf95485ed07068dc972c7fda5
Lindokuhle Mdlalose <mdllin028@myuct.ac.za>
2025-04-08 19:41:24 +0000

    Implemented the readPPMFile method, using the readPGMFile of the PGMimageProcessor class as the scope, but implementing in a way that it works for a ppm file

--- src/PPMimageProcessor.cpp
+++ src/PPMimageProcessor.cpp
@@ -43,3 +43,33 @@ PPMimageProcessor& PPMimageProcessor::operator=(PPMimageProcessor&& other) noexc
 unsigned char PPMimageProcessor::rgbToGrayScale(unsigned char r, unsigned char g, unsigned char b) {
     return static_cast<unsigned char>(0.299 * r + 0.587 * g + 0.114 * b);
 }
+
+bool PPMimageProcessor::readPPMFile(const std::string& filename){
+    std::ifstream file(filename, std::ios::binary);
+    if (!file){
+        std::cerr << "Error: Could not open file " << filename << std::endl;
+        return false;
+    }
+    
+    std::string magicNum;
+    file >> magicNum;
+    if (magicNum != "P6"){
+        std::cerr << "Error: Not a binary PPM file (P6)" << std::endl;
+        return false;
+    }
+
+    file >> width >> height;
+    int maxVal;
+    file >> maxVal;
+    file.get();
+
+    if (maxVal != 255){
+        std::cerr << "Warning: Unsupported max pixel value. Expected 255." << std::endl;
+    }
+
+    //delete[] imageData;
+    imageData = new unsigned char[width*height*3];
+    file.read(reinterpret_cast<char*>(imageData), width*height*3);
+
+    return file.good();
+}
